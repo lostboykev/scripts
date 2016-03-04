f05e67f534db0ca91ad266aadbd902c55747724d
lostboykev <lostboykev@gmail.com>
2016-03-04 10:04:33 -0600

    Update and rename ctload.sh to OpenVZ-ctload.sh

--- OpenVZ-ctload.sh
+++ OpenVZ-ctload.sh
@@ -1,3 +1,4 @@
+#OpenVZ first minute load average of all containers on node; sorted and ouput only load average of 2 and up
 echo "     CTID  LOAD"
 /usr/sbin/vzlist -o ctid,laverage | grep -v "CTID" | cut -d "/" -f1 | awk '$2>=2{print}'
 
